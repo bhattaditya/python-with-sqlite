d065903a921f518e817dcc20299729f93ad224df
Aditya Bhatt <bhattaditya2@gmail.com>
2021-11-22 19:57:50 +0530

    modifying_contact() modified - synchronized with database

--- Contact book/main.py
+++ Contact book/main.py
@@ -128,32 +128,49 @@ def deleting_contact(contact):
             conn.close()
 
 
-def modifying_contact(contact, conn):
-    if contact not in contact_list.keys():
-        return 0
-    else:
-        for index in contact_list.keys():
-            if index == contact:
+def modifying_contact(contact):
+    try:
+        conn = sqlite3.connect("./Contact book/contacts.sqlite")
+        cursor = conn.cursor()
+        sql = "Select index_value from contacts"
+        result = cursor.execute(sql)
+        modified = 0
+        for res in result:
+            if (contact,) == res:
+                time.sleep(2)
                 while True:
                     try:
                         edit = input("Do you want to edit email address or phone no (e/n): ")
                         if edit == 'n':
-                            
                             phone = phone_number()
                             sql1 = "update contacts set phone = ? where index_value = ?"
-                            conn.execute(sql1, (phone, contact,))
-                            contact_list[index][1] = phone  
+                            cursor.execute(sql1, (phone, contact,))
+                            # contact_list[contact][1] = phone  
                             conn.commit()
-                            break
+                            modified = 1
                         elif edit == 'e':
                             email = email_address()
                             sql1 = "update contacts set email = ? where index_value = ?"
-                            conn.execute(sql1, (email, contact,))
-                            contact_list[index][2] = email   
+                            cursor.execute(sql1, (email, contact,))
+                            # contact_list[contact][2] = email   
                             conn.commit()
-                            break  
+                            modified = 1
+
+                        if modified == 1:
+                            break
+
                     except ValueError as e:
                         print("please type 'e' or 'n'")
+                    
+                time.sleep(1)
+        return modified
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        if cursor:
+            cursor.close()
+        if conn:
+            conn.close()
 
 def show_contacts_length():
     try:
@@ -224,7 +241,7 @@ def menu():
 
             if choice == 3:
                 modify_ind = int(input("Enter index: "))
-                status = modifying_contact(modify_ind, conn)
+                status = modifying_contact(modify_ind)
                 time.sleep(1)
                 if status == 0:
                     print("Index not found...\n")
